3d565243051a37292264bae38e11b3b39a865b2e
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2024-12-10 12:59:38 -0300

    feat: add dbt models

---
+++ queries/models/adm_central_atendimento_1746/chamado_pessoa.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='table',
+        unique_key='id_chamado',
+        partition_by={
+            "field": "id_pessoa",
+            "data_type": "int64",
+        }
+    )
+}}
+
+SELECT 
+    SAFE_CAST(
+        REGEXP_REPLACE(id_chamado, r'\.0$', '') AS STRING
+    ) id_chamado,
+    SAFE_CAST(
+        REGEXP_REPLACE(id_pessoa_fk, r'\.0$', '') AS INT64
+    ) id_pessoa,
+FROM `rj-segovi.adm_central_atendimento_1746_staging.chamado_pessoa`
+WHERE id_pessoa_fk IS NOT NULL
