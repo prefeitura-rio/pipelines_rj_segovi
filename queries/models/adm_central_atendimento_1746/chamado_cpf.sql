5b492b587bddff477ca64437c32be9c70be8340a
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2024-12-10 16:17:39 -0300

    chore: expand data into columns

--- queries/models/adm_central_atendimento_1746/chamado_cpf.sql
+++ queries/models/adm_central_atendimento_1746/chamado_cpf.sql
@@ -9,16 +9,15 @@
 
 -- Full refresh or new rows for incremental runs
 with base_data as (
-    select
-        c.id_chamado,
+    select 
         p.cpf,
-        to_json_string(c) as data
+        c.*  -- Select all columns from the chamado table
     from `rj-segovi.adm_central_atendimento_1746.chamado` c
     left join `rj-segovi.adm_central_atendimento_1746.chamado_pessoa` cp
         on c.id_chamado = cp.id_chamado
     left join `rj-segovi.adm_central_atendimento_1746.pessoa` p
         on cp.id_pessoa = p.id_pessoa
-    where
+    where 
         c.id_chamado is not null
         and cp.id_pessoa is not null
         and p.cpf is not null
@@ -26,7 +25,7 @@ with base_data as (
 filtered_increment as (
     {% if is_incremental() %}
     -- Only add data not already in the target table
-    select *
+    select * 
     from base_data
     where id_chamado not in (
         select id_chamado
@@ -34,11 +33,11 @@ filtered_increment as (
     )
     {% else %}
     -- For full refreshes, include all rows
-    select *
+    select * 
     from base_data
     {% endif %}
 )
-select
+select 
     cpf,
-    data
+    * except(cpf) -- Include all columns except duplicating cpf
 from filtered_increment
